2693cc620c9c9a19b96a34a3ebf635e0ded8ec2d
ben-lawino <lawinwes@gmail.com>
2025-03-21 13:15:07 +0300

    added the results screen

---
+++ lib/questions_summary.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/data/questions.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  final List<Map<String, Object>> summaryData;
+
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children:
+              summaryData.map((data) {
+                final isCorrect = data['user_answer'] == data['correct_answer'];
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color:
+                            isCorrect
+                                ? Color.fromARGB(255, 150, 198, 241)
+                                : Color.fromARGB(255, 249, 133, 241),
+                      ),
+                      child: Center(
+                        child: Text(
+                          ((data['question_index'] as int) + 1).toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 22, 2, 56),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data['question'] as String,
+                            style: GoogleFonts.lato(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            data['user_answer'] as String,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 202, 171, 252),
+                            ),
+                          ),
+                          Text(
+                            data['correct_answer'] as String,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 181, 254, 246),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+        ),
+      ),
+    );
+  }
+}
